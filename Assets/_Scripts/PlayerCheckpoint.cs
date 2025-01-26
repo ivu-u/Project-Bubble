5c16943a144dd636de83f7f5c18a43d20ceebd6f
Carlijavier5 <carlijavier5@gmail.com>
2025-01-26 12:57:21 -0500

    Player Checkpoint + Kill Player + Add Bubble

---
+++ Assets/_Scripts/PlayerCheckpoint.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerCheckpoint : MonoBehaviour {
+
+	[SerializeField] private Transform checkpointTransform;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.TryGetComponent(out Player player)) {
+			player.SetCheckpoint(checkpointTransform);
+		}
+	}
+}
