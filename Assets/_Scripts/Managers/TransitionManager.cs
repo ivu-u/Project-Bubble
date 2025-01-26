4123ba6fcb2bbda39adcdc39810ab2afee8a92c7
Carlijavier5 <carlijavier5@gmail.com>
2025-01-26 16:49:53 -0500

    Bugfixes

--- Assets/_Scripts/Managers/TransitionManager.cs
+++ Assets/_Scripts/Managers/TransitionManager.cs
@@ -15,13 +15,14 @@ public class TransitionManager : MonoBehaviour {
     [SerializeField] private AnimationCurve timeScaleInCurve, timeScaleOutCurve;
     [SerializeField] private float softLoadTime, bubbleWait;
     [SerializeField] private SceneIdentifier[] sceneIDs;
+    [SerializeField] private FullScreenPassRendererFeature feature;
 
-    private readonly Dictionary<RoomTag, SceneRef> sceneMap = new();
+    private readonly Dictionary<RoomTag, SceneIdentifier> sceneMap = new();
 
     void Awake() {
         bubbleVFX.Stop();
         foreach (SceneIdentifier sceneID in sceneIDs) {
-            sceneMap[sceneID.roomTag] = sceneID.sceneRef;
+            sceneMap[sceneID.roomTag] = sceneID;
         }
     }
 
@@ -46,7 +47,9 @@ public class TransitionManager : MonoBehaviour {
     }
 
     private IEnumerator ILoadLevel(RoomTag roomTag) {
-        if (sceneMap.TryGetValue(roomTag, out SceneRef sceneRef)) {
+        if (sceneMap.TryGetValue(roomTag, out SceneIdentifier sceneID)) {
+            feature.passMaterial = sceneID.renderMat;
+            SceneRef sceneRef = sceneID.sceneRef;
             AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneRef.BuildIndex);
             yield return new WaitForSecondsRealtime(softLoadTime);
             while (!asyncOperation.isDone) yield return null;
@@ -64,4 +67,5 @@ public class TransitionManager : MonoBehaviour {
 public class SceneIdentifier {
     public RoomTag roomTag;
     public SceneRef sceneRef;
+    public Material renderMat;
 }
