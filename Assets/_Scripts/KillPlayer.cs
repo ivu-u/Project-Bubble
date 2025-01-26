0b96948030214747431197fbd2b6de88911a53c0
Carlijavier5 <carlijavier5@gmail.com>
2025-01-26 14:05:02 -0500

    Transition Bubbles

--- Assets/_Scripts/KillPlayer.cs
+++ Assets/_Scripts/KillPlayer.cs
@@ -2,7 +2,13 @@ using UnityEngine;
 
 public class KillPlayer : MonoBehaviour {
 
-	void OnTriggerEnter(Collider other) {
+    void OnCollisionEnter(Collision info) {
+		if (info.collider.TryGetComponent(out Player player)) {
+			player.Kill();
+		}
+	}
+
+    void OnTriggerEnter(Collider other) {
 		if (other.TryGetComponent(out Player player)) {
 			player.Kill();
 		}
