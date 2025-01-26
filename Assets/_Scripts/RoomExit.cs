e352ea6475d738e9b7585ad3b7c6098e2467b474
Carlijavier5 <carlijavier5@gmail.com>
2025-01-26 13:14:53 -0500

    Room Asset Prefabs

---
+++ Assets/_Scripts/RoomExit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RoomExit : MonoBehaviour {
+
+	[SerializeField] private RoomTag roomTag;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.TryGetComponent(out Player _)) {
+			GM.TransitionManager.LoadLevel(roomTag);
+		}
+	}
+}
